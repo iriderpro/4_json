12687281c29906e6b0dd64f8fd356a39871e9f25
iriderpro <irider.pro@gmail.com>
2017-12-21 17:38:24 +0300

    Написана программа

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
 import json
+import codecs
+import pprint
 
 
 def load_data(filepath):
-    pass
+    fileObj = codecs.open(filepath, "r", "utf_8_sig")
+    json_data = fileObj.read()
+    data = json.loads(json_data)
+    return (data)
 
 
 def pretty_print_json(data):
-    pass
-
+    pprint.pprint(data)
 
 if __name__ == '__main__':
-    pass
+    N = input('введите путь к файлу : ')
+    pprint.pprint(load_data(N))
