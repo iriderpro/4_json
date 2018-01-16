569f6ea4fa4f77384e3845493c31d17249b8a2a1
iriderpro <irider.pro@gmail.com>
2018-01-16 10:01:12 +0300

    Update pprint_json.py
    Rename function

--- pprint_json.py
+++ pprint_json.py
@@ -3,32 +3,23 @@ import sys
 import os
 
 
-def load_data(filepath):
-
+def deserialize_fp_json(filepath):
     with open(filepath, "r", encoding='utf8', errors='ignore') as file_obj:
-
         json_load = json.load(file_obj)
-
     return json_load
 
 
-def pretty_print_json(json_obj):
-
-    json_dumps = json.dumps(json_obj, ensure_ascii=False, indent=4, sort_keys=True)
-
+def serialize_obj_json(json_obj):
+    json_dumps = json.dumps(json_obj, ensure_ascii=False,
+                            indent=4, sort_keys=True)
     return json_dumps
 
 
 if __name__ == '__main__':
-
     if len(sys.argv) > 1:
-
         if os.path.exists(sys.argv[1]):
-
-            print (pretty_print_json(load_data(sys.argv[1])))
-
+            print (serialize_obj(deserialize_fp(sys.argv[1])))
         else:
             print("файла не существует")
-
     else:
         print ("нет входного файла")
