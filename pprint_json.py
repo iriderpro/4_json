3af4882abaa6176b31c5f779c39a6e51fdbdb6a7
iriderpro <irider.pro@gmail.com>
2018-01-18 11:30:54 +0300

    Update pprint_json.py
    change " to '
    format list

--- pprint_json.py
+++ pprint_json.py
@@ -4,14 +4,18 @@ import os
 
 
 def load_data(filepath):
-    with open(filepath, "r", encoding='utf8') as file_obj:
+    with open(filepath, 'r', encoding='utf8') as file_obj:
         python_obj = json.load(file_obj)
     return python_obj
 
 
 def prettify_json(py_obj):
-    json_str = json.dumps(py_obj, ensure_ascii=False,
-                          indent=4, sort_keys=True)
+    json_str = json.dumps(
+        py_obj,
+        ensure_ascii=False,
+        indent=4,
+        sort_keys=True,
+    )
     return json_str
 
 
@@ -20,6 +24,6 @@ if __name__ == '__main__':
         if os.path.exists(sys.argv[1]):
             print (prettify_json(load_data(sys.argv[1])))
         else:
-            print("файла не существует")
+            print('файла не существует')
     else:
-        print ("нет входного файла")
+        print ('нет входного файла')
